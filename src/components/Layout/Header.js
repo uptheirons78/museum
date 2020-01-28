3e5a2353a09030d54eb303ed101113e3e8e7a2b2
Mauro Bono <bono.mauro@gmail.com>
2020-01-28 11:27:14 +0100

    Added SkrewedSection Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,53 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import SkrewedSection from "../Shared/SkrewedSection";
+
+// Images
+import SkrewedImage from "../../images/header_banner.jpg";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <section className="slanted">
-        <div className="left">
-          <h1>Museo Civico di Nepi</h1>
-        </div>
-        <div className="right"></div>
-      </section>
+      <SkrewedSection
+        title="Museo Civico di Nepi"
+        bgColor="#B20932"
+        polygon="polygon(0 0, 100% 0, 95% 100%, 0% 100%)"
+        height="180px"
+        contentWidth="400px"
+        pseudoContentWidth="395px"
+        img={SkrewedImage}
+      />
     </StyledHeader>
   );
 };
 
 export default Header;
 
-const StyledHeader = styled.header`
-  .slanted {
-    background: ${props => props.theme.primaryColor};
-    width: 100%;
-    height: 120px;
-    display: flex;
-
-    .left {
-      width: 35%;
-      height: 100%;
-      background: ${props => props.theme.primaryColor};
-      /* transform: translateX(5px); */
-      clip-path: polygon(0 0, 100% 0, 96% 100%, 0% 100%);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: #fff;
-
-      h1 {
-        text-transform: uppercase;
-      }
-    }
-
-    .right {
-      width: 65%;
-      height: 100%;
-      background: url("http://www.museodiromaintrastevere.it/sites/default/files/styles/large/public/storage/images/musei/museo_di_roma_in_trastevere/il_museo/editoriale/2484-25-ita-IT/editoriale.jpg?itok=HYHyyEZj");
-      background-repeat: none;
-      background-position: center center;
-      background-size: cover;
-      /* transform: translateX(-5px); */
-      clip-path: polygon(2% 0, 100% 0, 100% 100%, 0% 100%);
-    }
-  }
-`;
+const StyledHeader = styled.header``;
---
+++ src/components/Shared/SkrewedSection.js
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components";
+
+const SkrewedSection = ({
+  title,
+  bgColor,
+  polygon,
+  height,
+  contentWidth,
+  pseudoContentWidth,
+  img,
+}) => {
+  return (
+    <StyledSkrewedSection
+      className="skrewed-section"
+      bgColor={bgColor || "#B20932"}
+      polygon={polygon || "polygon(0 0, 100% 0, 95% 100%, 0% 100%)"}
+      height={height || "150px"}
+      contentWidth={contentWidth || "400px"}
+      pseudoContentWidth={pseudoContentWidth || "395px"}
+      img={img}
+    >
+      <div className="skrewed-section__content">
+        <h1 className="skrewed-section__content-title">{title}</h1>
+      </div>
+    </StyledSkrewedSection>
+  );
+};
+
+export default SkrewedSection;
+
+const StyledSkrewedSection = styled.section`
+  width: 100%;
+  height: ${props => props.height};
+  margin: 0 auto;
+  background-image: url(${props => props.img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+
+  @media screen and (max-width: 600px) {
+    height: 75px;
+  }
+
+  .skrewed-section__content {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: ${props => props.contentWidth};
+    height: 100%;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    clip-path: ${props => props.polygon};
+
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: ${props => props.pseudoContentWidth};
+      height: 100%;
+      background: ${props => props.bgColor};
+      clip-path: ${props => props.polygon};
+      z-index: -1;
+    }
+
+    .skrewed-section__content-title {
+      color: #fff;
+      text-transform: uppercase;
+    }
+  }
+`;
